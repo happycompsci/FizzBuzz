16c0d357b16bb3f260d68e8cc685f3bf38f756cd
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-09-12 22:22:28 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -32,3 +32,40 @@ class FizzBuzz {
         }
     }
 }
+
+
+/**
+ * Solve the FizzBuzz challenge.
+ */
+class DoFizzBuzz {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i < 100) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
