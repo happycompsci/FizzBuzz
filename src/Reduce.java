8d0e83007f247c879a1a395a6c49d65f298f1554
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-09-13 09:06:46 -0400

    finally completed

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int initial = 100;
+        while (initial != 0) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy2 = initial % 2 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy2) {
+
+                initial = initial / 2;
+                count = count + 1;
+
+            } else if (!divisibleBy2){
+
+                initial = initial - 1;
+                count = count + 1;
+            }
+    }System.out.println(count);
+}
 }
